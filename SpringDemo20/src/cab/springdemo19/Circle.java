1f5419058d5128b9c7210b58ff6bbf351fd25763
Carlos Borrero <cborrero2000@hotmail.com>
2015-04-08 01:17:46 -0400

    First commit of App testing JSR-250 Annotations @Resource @PostConstruct @PreDestroy

---
+++ SpringDemo20/src/cab/springdemo19/Circle.java
@@ -0,0 +1,40 @@
+package cab.springdemo19;
+
+import static cb.printscreen.PSLog.*;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Circle implements Shape {
+
+	private Point center = new Point();
+
+	public Point getCenter() {
+		return center;
+	}
+
+	@Resource
+	public void setCenter(Point center) {
+		this.center = center;
+	}
+
+	@Override
+	public void draw() {
+		ps("Center: x=" + center.getX() + " y=" + center.getY());
+
+	}
+	
+	@PostConstruct
+	public void initializeCircle() {
+		ps("Circle is being initialized");
+	}
+	
+	@PreDestroy
+	public void destroyCircle() {
+		ps("Circle is being destroyed");
+	}
+}
