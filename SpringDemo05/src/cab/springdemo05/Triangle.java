8f61c2977e47f09a7e45bd6ac664d2d9fe7e0ce5
Carlos Borrero <cborrero2000@hotmail.com>
2015-04-27 23:23:27 -0400

    Initial commit of SpringDemo05

---
+++ SpringDemo05/src/cab/springdemo05/Triangle.java
@@ -0,0 +1,18 @@
+package cab.springdemo05;
+
+public class Triangle {
+	
+	private String type;
+	
+	public void draw() {
+		System.out.println(getType() + " Triangle Drawn");
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
