40cdf3dbb85792898a82c3db1e4b7adb9b839562
Carlos Borrero <cborrero2000@hotmail.com>
2015-04-28 23:52:23 -0400

    Inner Beans, Aliases and name in Bean declaration

---
+++ SpringDemo08/src/cab/springdemo08/DrawingApp.java
@@ -0,0 +1,14 @@
+package cab.springdemo08;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//		Triangle triangle = (Triangle) context.getBean("triangle");
+//		Triangle triangle = (Triangle) context.getBean("triangle-alias");
+		Triangle triangle = (Triangle) context.getBean("triangle-name");
+		triangle.draw();
+	}
+}
