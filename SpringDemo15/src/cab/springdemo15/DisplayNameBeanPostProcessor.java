acb77310e1d58f6fb54d2df6954e1baeb837e720
Carlos Borrero <cborrero2000@hotmail.com>
2015-05-03 17:51:33 -0400

    Spring Tutorial 15 - Writing a BeanPostProcessor

---
+++ SpringDemo15/src/cab/springdemo15/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package cab.springdemo15;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("Executing postProcessAfterInitialization for Bean: " + beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("Executing postProcessAfterInitialization for Bean: " + beanName);
+		return bean;
+	}
+
+}
